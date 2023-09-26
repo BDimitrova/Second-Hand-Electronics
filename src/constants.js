5b60622bd05c529e072a3f09a5f11ff4d167a12b
Boryana Dimitrova <b.dimitrova97@gmail.com>
2023-09-26 15:45:16 +0300

    Chain mongoose to server

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 exports.PORT = 3000;
+exports.DB_Connection_String = 'mongodb://localhost:27017/second-hand-electronics';
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 const { PORT } = require('./constants');
 const routes = require('./routes');
+const initDatabase = require('./config/mongooseConfig');
 
 require('./config/hbsConfig')(app);
 require('./config/expressConfic')(app);
 
 app.use(routes);
 
-app.listen(PORT, () => console.log(`The app is running on port ${PORT}...`));
+initDatabase()
+    .then(() => {
+        app.listen(PORT, () => console.log(`The app is listening on port http://localhost:${PORT}`));
+    })
+    .catch((err) => {
+        console.log('Connot connect database:' + err);
+    })
