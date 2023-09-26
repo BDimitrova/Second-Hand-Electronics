03d40bf2bc03d1b82badd072c074691636ca7e62
Boryana Dimitrova <b.dimitrova97@gmail.com>
2023-09-26 16:22:02 +0300

    Chain login and register page

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const homeController = require('./controller/homeController');
+const authController = require('./controller/authController');
 
 router.use(homeController);
+router.use('/auth/', authController);
 
 module.exports = router;
