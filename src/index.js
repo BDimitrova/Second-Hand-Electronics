0c4b687bcb4903c7db4aefd2237ae31737243f26
Boryana Dimitrova <b.dimitrova97@gmail.com>
2023-09-26 15:28:39 +0300

    Chain css files

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+require('./config/hbsConfig')(app);
+require('./config/expressConfic')(app);
+
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.render('home');
 });
 
 app.listen(port, () => console.log(`The app is running on port ${port}...`));
